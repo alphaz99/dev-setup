6675a77d4f93377df2f1ee65a3f46de496cdc13c
Rahul Bachal <rbachal@caltech.edu>
2016-04-06 15:06:20 -0700

    Removing old zshrc

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,6 +1,9 @@
 # Clone the prezto repo
 git clone --recursive https://github.com/alphaz99/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
+# Remove previous zsh configuration
+rm ~/.zshrc
+
 # Install prezto
 zsh install_prezto.sh
 
