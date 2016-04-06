ac69c33258a6dbcb5df26ee84d3e7ba68316eff0
Rahul Bachal <rbachal@caltech.edu>
2016-04-06 16:58:41 -0700

    Added option to install fonts

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -7,9 +7,16 @@ rm ~/.zshrc
 # Install prezto
 zsh install_prezto.sh
 
-# Install fonts
-git clone https://github.com/powerline/fonts.git "$HOME/fonts"
-~/fonts/install.sh
+read -p "Do you want to install powerline fonts? " yn
+case $yn in
+    [Yy]* )
+        # Install fonts
+        git clone https://github.com/powerline/fonts.git "$HOME/fonts"
+        ~/fonts/install.sh
+        break;;
+    [Nn]* )
+        ;;
+esac
 
 # Set Zsh as the default shell
 chsh -s /bin/zsh
