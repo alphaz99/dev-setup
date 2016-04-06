28c2d8696fb8f6d1bc3654145b6fc209c14a688c
Rahul Bachal <rbachal@caltech.edu>
2016-04-06 14:33:08 -0700

    Added zsh configuration setup

---
+++ zsh_setup.sh
@@ -0,0 +1,11 @@
+# Clone the prezto repo
+git clone --recursive https://github.com/alphaz99/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Copy the configuration files
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+
+# Set Zsh as the default shell
+chsh -s /bin/zsh
