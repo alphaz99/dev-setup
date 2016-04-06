990e703e390c2180e05f242cafc4554fae763d62
Rahul Bachal <rbachal@caltech.edu>
2016-04-06 15:03:21 -0700

    Added script to install prezto

---
+++ install_prezto.sh
@@ -0,0 +1,5 @@
+# Copy the configuration files
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
--- zsh_setup.sh
+++ zsh_setup.sh
@@ -4,11 +4,8 @@ zsh
 # Clone the prezto repo
 git clone --recursive https://github.com/alphaz99/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-# Copy the configuration files
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-  done
+# Install prezto
+zsh install_prezto.sh
 
 # Set Zsh as the default shell
 chsh -s /bin/zsh
