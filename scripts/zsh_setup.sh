00001a74f79f566ca6fd6bb6c056e9786dbf4ce8
Rahul Bachal <rbachal@fb.com>
2020-09-14 19:37:07 -0700

    Fix

--- scripts/zsh_setup.sh
+++ scripts/zsh_setup.sh
@@ -1,12 +1,12 @@
 # Check if zsh is installed
 command -v zsh >/dev/null 2>&1 || { echo >&2 "I require zsh but it's not installed.  Aborting."; exit 1; }
 
-# Install zinit
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
 # Remove previous zsh configuration
 if [ -e ~/.zshrc ] || [ -L ~/.zshrc ]; then mv ~/.zshrc ~/.zshrc_bak; fi
 if [ -e ~/.zshenv ] || [ -L ~/.zshenv ]; then mv ~/.zshenv ~/.zshenv_bak; fi
+
+# Install zinit
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 # Symlink new zsh configuration
 mkdir -p $HOME/.zsh
@@ -15,7 +15,7 @@ ln -s $(pwd)/zsh/.zshenv_home ~/.zshenv
 ln -s $(pwd)/zsh/.zshenv ~/.zsh/.zshenv
 
 
-# Remove previous zsh configuration
+# Remove previous powerlevel10k configuration
 if [ -e ~/.p10k.zsh ] || [ -L ~/.p10k.zsh ]; then mv ~/.p10k.zsh ~/.p10k.zsh; fi
 
 # Symlink powerlevel10k configuration
